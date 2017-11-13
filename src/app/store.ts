ac020ff5e8a2c80f5ace49b49e98946f9299a9b1
sup3rcow <bullandrooster@gmail.com>
2017-11-13 16:40:50 +0100

    rad1

--- src/app/store.ts
+++ src/app/store.ts
@@ -15,9 +15,10 @@ export const INITIAL_STATE: IAppState = {
 export function rootReducer(state: IAppState, action) {
     switch (action.type) {
         case ADD_TODO:
+        let newTodo = { id: this.todos.length + 1, title: action.title };
         return tassign(state,
             {
-                todos: state.todos.concat(state),
+                todos: state.todos.concat(newTodo),
                 lastUpdate : new Date()
             }
         );
